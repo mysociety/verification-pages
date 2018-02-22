45957fdb695974c32dc451941b2a86d6d039f6a4
Graeme Porteous <graeme@rgbp.co.uk>
2018-02-22 13:06:04 +0000

    Split HTML layout and template
    So it can be compiled into the JS production bundle

---
+++ app/javascript/app.js
@@ -0,0 +1,27 @@
+import Axios from 'axios'
+import template from './app.html'
+
+export default template({
+  data () {
+    return {
+      loaded: false,
+      statements: [],
+      statementIndex: 0
+    }
+  },
+  created: function () {
+    this.loadStatements()
+  },
+  methods: {
+    statement: function () {
+      return this.statements[this.statementIndex]
+    },
+    loadStatements: function () {
+      Axios.get('/statements/1.json').then(response => {
+        this.statements = response.data.statements
+      }).then(() => {
+        this.loaded = true
+      })
+    }
+  }
+})

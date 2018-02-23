d917bd5cb2fcbfd68d6b933da2faf5d68e071e84
Graeme Porteous <graeme@rgbp.co.uk>
2018-02-23 17:29:49 +0000

    WIP Starting to implement searching with Wiki API

--- app/javascript/components/reconcilable.js
+++ app/javascript/components/reconcilable.js
@@ -1,5 +1,15 @@
+import wikidataClient from '../wikiapi.js'
 import template from './reconcilable.html'
 
 export default template({
-  data () { return {} }
+  data () { return {} },
+  props: ['statement'],
+  methods: {
+    searchForName: function () {
+      const name = this.statement.person_name
+      wikidataClient.search(name, 'en', 'en').then(data => {
+        console.log(data)
+      })
+    }
+  }
 })

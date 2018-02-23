5568bb4b9188a3dda88c67019e7f0d6ade535b38
Graeme Porteous <graeme@rgbp.co.uk>
2018-02-23 10:53:24 +0000

    Switch to loading frontend on window load event

--- app/javascript/packs/verification_tool.js
+++ app/javascript/packs/verification_tool.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from '../app'
 
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
   const el = document.getElementById('mw-content-text')
                      .appendChild(document.createElement('hello'))
   const app = new Vue({ el, render: h => h(App) })

7a6835f55eb8ae058991fccc28dad09897889114
Graeme Porteous <graeme@rgbp.co.uk>
2018-02-23 15:19:45 +0000

    Return updated statement JSON after verification

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -6,8 +6,17 @@ class VerificationsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    statement = Statement.find_by(transaction_id: params.fetch(:id))
+    transaction_id = params.fetch(:id)
+    statement = Statement.find_by(transaction_id: transaction_id)
     statement.create_verification!(verification_params)
+
+    page = statement.page
+
+    @classifier = StatementClassifier.new(page.title, transaction_id)
+
+    respond_to do |format|
+      format.json { render file: 'statements/show' }
+    end
   end
 
   private

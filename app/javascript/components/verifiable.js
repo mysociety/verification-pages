0122fe860a44dc3745e832d47a08264459671696
Mark Longair <mhl@pobox.com>
2018-02-23 16:05:54 +0000

    More WIP - switch to emitting an update-statement event

--- app/javascript/components/verifiable.js
+++ app/javascript/components/verifiable.js
@@ -1,24 +1,25 @@
+import Axios from 'axios'
 import template from './verifiable.html'
 
 export default template({
-  data () { return {} },
+  data () { return {
+    submitting: false
+  } },
   props: ['statement'],
   methods: {
     verifyStatement: function () {
       this.submitting = true
       const data = {
-        id: statement.transaction_id,
+        id: this.statement.transaction_id,
         user: 'ExampleUser',
         status: true
       }
-      const currentIndex = this.statementIndex
       Axios.post('/verifications.json', data).then(response => {
         if (response.data.statements.length > 1) {
           throw 'Response has too many statements. We don\'t know which one to update'
         }
-
         const newStatement = response.data.statements[0]
-        this.statements[currentIndex] = newStatement
+        this.$emit('statement-update', newStatement)
         this.submitting = false
       })
     }

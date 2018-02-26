61b551d74ee6c52a7e42389afe450d67a752f0fc
Graeme Porteous <graeme@rgbp.co.uk>
2018-02-26 14:55:20 +0000

    Simplify verification controller
    Extract methods into a frontend superclass to be used by other
    controllers in the future

---
+++ app/controllers/frontend_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Controller to receive POST verification requests from verification tool on
+# Wikidata
+class FrontendController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def respond_with(statement)
+    page = statement.page
+    @classifier = StatementClassifier.new(page.title, statement.transaction_id)
+
+    respond_to do |format|
+      format.json { render file: 'statements/show' }
+    end
+  end
+end
--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -1,22 +1,13 @@
 # frozen_string_literal: true
 
-# Controller to receive POST reconciliation requests from verification pages on
+# Controller to receive POST verification requests from verification tool on
 # Wikidata
-class VerificationsController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
+class VerificationsController < FrontendController
   def create
-    transaction_id = params.fetch(:id)
-    statement = Statement.find_by(transaction_id: transaction_id)
+    statement = Statement.find_by(transaction_id: params.fetch(:id))
     statement.create_verification!(verification_params)
 
-    page = statement.page
-
-    @classifier = StatementClassifier.new(page.title, transaction_id)
-
-    respond_to do |format|
-      format.json { render file: 'statements/show' }
-    end
+    respond_with(statement)
   end
 
   private

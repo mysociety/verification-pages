6f7384070113fbd3139e3ac6f2d67fce25aa33f3
Graeme Porteous <graeme@rgbp.co.uk>
2018-02-26 20:01:39 +0000

    Insert tool just under the main Wikipage H1

--- app/javascript/packs/verification_tool.js
+++ app/javascript/packs/verification_tool.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from '../app'
 
 window.addEventListener('load', () => {
-  const el = document.getElementById('mw-content-text')
-                     .appendChild(document.createElement('hello'))
+  const el = document.createElement('verification-tool')
+  const spacer = document.createElement('br')
+  const heading = document.querySelector('#mw-content-text h1')
+
+  heading.parentNode.insertBefore(spacer, heading.nextSibling)
+  heading.parentNode.insertBefore(el, spacer.nextSibling)
+
   const app = new Vue({ el, render: h => h(App) })
 })

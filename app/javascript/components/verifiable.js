03c8cda2d1030eb0760cd9fb847d11d65a8222ef
Graeme Porteous <graeme@rgbp.co.uk>
2018-02-26 13:55:35 +0000

    Extract submitting conditional

--- app/javascript/components/verifiable.js
+++ app/javascript/components/verifiable.js
@@ -3,25 +3,16 @@ import wikidataClient from '../wikiapi.js'
 import template from './verifiable.html'
 
 export default template({
-  data () { return {
-    submitting: false
-  } },
+  data () { return {} },
   props: ['statement'],
   methods: {
     verifyStatement: function () {
-      this.submitting = true
-      const data = {
-        id: this.statement.transaction_id,
-        user: wikidataClient.user,
-        status: true
-      }
-      Axios.post('/verifications.json', data).then(response => {
-        if (response.data.statements.length > 1) {
-          throw 'Response has too many statements. We don\'t know which one to update'
-        }
-        const newStatement = response.data.statements[0]
-        this.$parent.$emit('statement-update', newStatement)
-        this.submitting = false
+      this.$parent.$emit('statement-update', () => {
+        return Axios.post('/verifications.json', {
+          id: this.statement.transaction_id,
+          user: wikidataClient.user,
+          status: true
+        })
       })
     }
   }

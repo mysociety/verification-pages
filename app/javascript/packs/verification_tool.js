eb78f6cf490ed5b525aafce306a9bd5859e304ab
Graeme Porteous <graeme@rgbp.co.uk>
2018-02-27 14:50:37 +0000

    Bind Vue to a known div ID

--- app/javascript/packs/verification_tool.js
+++ app/javascript/packs/verification_tool.js
@@ -2,12 +2,8 @@ import Vue from 'vue'
 import App from '../app'
 
 window.addEventListener('load', () => {
-  const el = document.createElement('verification-tool')
-  const spacer = document.createElement('br')
-  const heading = document.querySelector('#mw-content-text h1')
-
-  heading.parentNode.insertBefore(spacer, heading.nextSibling)
-  heading.parentNode.insertBefore(el, spacer.nextSibling)
+  const el = document.getElementById('js-verification-tool')
+                     .appendChild(document.createElement('verification-tool'))
 
   const app = new Vue({ el, render: h => h(App) })
 })

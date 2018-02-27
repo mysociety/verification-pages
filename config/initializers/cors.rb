7b164424c3e5f06aba8812ccefb6d5485b49e319
Graeme Porteous <graeme@rgbp.co.uk>
2018-02-27 15:02:11 +0000

    Update CORS headers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,9 +2,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins %r{\Ahttps://(www|test)\.wikidata\.org\z}
-    resource %r{\A/statements/}, headers: :any, methods: %i[get options]
-    resource '/verifications', headers: :any, methods: %i[post options]
-    resource '/reconciliations', headers: :any, methods: %i[post options]
+    origins  %r{\Ahttps://(www|test)\.wikidata\.org\z}
+    resource %r{\A/statements}, headers: :any, methods: %i[get options]
+    resource %r{\A/verifications}, headers: :any, methods: %i[post options]
+    resource %r{\A/reconciliations}, headers: :any, methods: %i[post options]
   end
 end

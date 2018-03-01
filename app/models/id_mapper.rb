d72ea5293dbf48e78a5464048f5dd52c02374165
Graeme Porteous <graeme@rgbp.co.uk>
2018-03-01 13:05:05 +0000

    Send equivalence claims to mapping store

--- app/models/id_mapper.rb
+++ app/models/id_mapper.rb
@@ -4,27 +4,23 @@ require 'rest-client'
 require 'securerandom'
 
 class IDMapper
-  def ms_uuid_for(collection, wikidata_id, comment_for_creation = '')
-    existing = existing_wd_to_ms_uuid[collection][wikidata_id]
-    return existing if existing
-    new_ms_uuid_for(collection, wikidata_id, comment_for_creation)
+  COLLECTION = :persons
+
+  def fb_id_for(wikidata_id)
+    existing_wd_to_fb_id[COLLECTION][wikidata_id]
   end
 
-  private
-
-  def new_ms_uuid_for(collection, wikidata_id, comment_for_creation)
-    ms_uuid = "urn:uuid:#{SecureRandom.uuid}"
-    payload = {
+  def new_fb_id_for(facebook_id, wikidata_id, payload = {})
+    payload = payload.merge(
       identifier_a: {
-        scheme_id: SCHEMA_WIKIDATA.fetch(collection),
+        scheme_id: SCHEMA_WIKIDATA.fetch(COLLECTION),
         value: wikidata_id,
       },
       identifier_b: {
-        scheme_id: SCHEMA_MS_UUID.fetch(collection),
-        value: ms_uuid,
+        scheme_id: SCHEMA_FD_ID.fetch(COLLECTION),
+        value: facebook_id,
       },
-      comment: comment_for_creation,
-    }.to_json
+    ).to_json
     post_url = "#{ID_MAPPING_STORE_BASE_URL}/equivalence-claim"
     headers = { content_type: :json, accept: :json, x_api_key: api_key }
     response = RestClient.post post_url, payload, headers
@@ -33,23 +29,25 @@ class IDMapper
     end
     # Update the existing store with the newly created ID before
     # returning it:
-    existing_wd_to_ms_uuid[collection][wikidata_id] = ms_uuid
-    ms_uuid
+    existing_wd_to_fb_id[COLLECTION][wikidata_id] = facebook_id
+    facebook_id
   end
 
-  def existing_wd_to_ms_uuid
-    @existing_wd_to_ms_uuid ||= COLLECTIONS.map do |collection|
-      [collection, existing_wd_to_ms_uuid_for(collection)]
+  private
+
+  def existing_wd_to_fb_id
+    @existing_wd_to_fb_id ||= COLLECTIONS.map do |collection|
+      [collection, existing_wd_to_fb_id_for(collection)]
     end.to_h
   end
 
-  def existing_wd_to_ms_uuid_for(collection)
+  def existing_wd_to_fb_id_for(collection)
     url = "#{ID_MAPPING_STORE_BASE_URL}/scheme/#{SCHEMA_WIKIDATA.fetch(collection)}"
     data = JSON.parse(open(url, &:read))['results']
     data.map do |k, other_identifiers|
       [
         k,
-        other_identifiers.find { |i| i['scheme_id'] == SCHEMA_MS_UUID[collection] }&.fetch('value'),
+        other_identifiers.find { |i| i['scheme_id'] == SCHEMA_FD_ID[collection] }&.fetch('value'),
       ]
     end.to_h
   end
@@ -64,16 +62,12 @@ class IDMapper
 
   ID_MAPPING_STORE_BASE_URL = 'https://id-mapping-store.mysociety.org'
 
-  COLLECTIONS = %i(persons organizations memberships).freeze
+  COLLECTIONS = %i(persons).freeze
 
   SCHEMA_WIKIDATA = {
     persons: 1,
-    memberships: 2,
-    organizations: 3,
   }.freeze
-  SCHEMA_MS_UUID = {
-    persons: 4,
-    memberships: 5,
-    organizations: 6,
+  SCHEMA_FD_ID = {
+    persons: 7,
   }.freeze
 end

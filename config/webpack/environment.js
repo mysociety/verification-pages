f5b5dd8da709490546ce92ff03309c7e400f6c7e
Graeme Porteous <graeme@rgbp.co.uk>
2018-04-06 10:41:11 +0100

    Enable Webpack CSS loader
    This allows CSS or SCSS to be compiled into the tool's JS using an
    import EG:
      `import template from './app.html?style=./app.css.scss'`
    
    Had to remove the default Webpacker CSS/SASS loaders due to:
      https://github.com/webpack-contrib/sass-loader/issues/536

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
 const { environment } = require('@rails/webpacker')
 const vueTemplate = require('./loaders/vue_template')
 
-environment.loaders.append('html', vueTemplate)
+environment.loaders.delete('css')
+environment.loaders.delete('sass')
+environment.loaders.append('html', vueTemplate.html)
+environment.loaders.append('css', vueTemplate.css)
+
 module.exports = environment

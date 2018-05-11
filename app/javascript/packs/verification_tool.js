4ba27145b73f5e83024a5fc6815ac985c0a4412f
Mark Longair <mhl@pobox.com>
2018-05-11 17:29:53 +0100

    After creating the Vue app, remove the "install our JavaScript" prompt
    It was weird that the page was still telling you to install the
    JavaScript extension even after you'd done that successfully. This
    change means it'll be removed if the JavaScript is loaded succesfully.
    
    Fixes #70

--- app/javascript/packs/verification_tool.js
+++ app/javascript/packs/verification_tool.js
@@ -2,6 +2,19 @@ import Vue from 'vue'
 import App from '../app'
 import wikilink from '../components/wikilink'
 
+function removeInstallPrompt() {
+  var installPrompt = document.getElementById('js-verification-tool');
+  // It'd be more robust to get the install prompt by a special class
+  // we add to it, but the version of the cmbox template on Wikidata
+  // doesn't have the 'class' parameter, so do this another way:
+  while (installPrompt && installPrompt.className != 'layouttemplate plainlinks') {
+    installPrompt = installPrompt.nextElementSibling;
+  }
+  if (installPrompt) {
+    installPrompt.style.display = 'none';
+  }
+}
+
 Vue.component('wikilink', wikilink)
 
 window.addEventListener('load', () => {
@@ -9,4 +22,5 @@ window.addEventListener('load', () => {
                      .appendChild(document.createElement('verification-tool'))
 
   const app = new Vue({ el, render: h => h(App) })
+  removeInstallPrompt();
 })

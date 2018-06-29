178bcfe300e6d28d805cb80e1b6b9263d91dbd4f
Graeme Porteous <graeme@rgbp.co.uk>
2018-06-29 14:44:18 +0100

    Use the term in the RetrievePositionData query
    If we have a verification page with a term and there are existing
    positions on Wikidata with start dates we can ignore any if the term
    didn't start before the position.
    
    This means we never have the old position statement from a previous
    term. Instead the app will have to create a new position statement when
    the statement is actioned.
    
    Example query: http://tinyurl.com/y7onjruh

--- app/services/retrieve_position_data.rb
+++ app/services/retrieve_position_data.rb
@@ -28,6 +28,7 @@ class RetrievePositionData < ServiceBase
       SELECT DISTINCT ?person ?merged_then_deleted ?revision ?position ?start_of_term ?start_date ?term ?group ?district
       WHERE {
         %<person_bind>s
+        BIND(wd:#{parliamentary_term_item} AS ?page_term)
         ?position ps:P39 wd:%<position_held_item>s .
         ?person wdt:P31 wd:Q5 ; p:P39 ?position .
         ?person schema:version ?revision .
@@ -35,10 +36,15 @@ class RetrievePositionData < ServiceBase
           ?position pq:P2937 ?term .
           OPTIONAL { ?term (wdt:P571|wdt:P580) ?start_of_term . }
         }
+        OPTIONAL { ?page_term (wdt:P571|wdt:P580) ?start_of_page_term . }
         OPTIONAL { ?position pq:P4100 ?group . }
         OPTIONAL { ?position pq:P768 ?district . }
         OPTIONAL { ?position pq:P580 ?start_date . }
         OPTIONAL { ?merged_then_deleted owl:sameAs ?person }
+        FILTER (
+          !bound(?start_of_page_term) || !bound(?start_date) ||
+          ?start_of_page_term < ?start_date
+        )
       }
     SPARQL
   end

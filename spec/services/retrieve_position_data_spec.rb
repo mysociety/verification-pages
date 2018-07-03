6d012a3cb1ee92387d3c8de55b39d5fdb1f67301
Mark Longair <mhl@pobox.com>
2018-07-03 11:42:44 +0100

    RetrievePositionData: make the query method public
    We'd like to be able to make it easy to run the query for finding
    existing position in the Wikidata Query Service; but previously you
    couldn't get the query that'll be used without running it. (Also the
    existing private query method only returned the templated query.)
    
    This commit factors out a public method that returns the query that
    would actually be run.

--- spec/services/retrieve_position_data_spec.rb
+++ spec/services/retrieve_position_data_spec.rb
@@ -21,20 +21,20 @@ RSpec.describe RetrievePositionData, type: :service do
 
   describe '#run' do
     it 'calls run_query with substituted position_held_item' do
-      allow(service).to receive(:query).and_return('%<position_held_item>s')
+      allow(service).to receive(:query_format).and_return('%<position_held_item>s')
       expect(service).to receive(:run_query).with('Q1')
       service.run
     end
 
     it 'calls run_query with substituted parliamentary_term_item' do
-      allow(service).to receive(:query).and_return('%<parliamentary_term_item>s')
+      allow(service).to receive(:query_format).and_return('%<parliamentary_term_item>s')
       expect(service).to receive(:run_query).with('Q2')
       service.run
     end
 
     context 'with person_item' do
       it 'calls run_query with substituted person_bind' do
-        allow(service).to receive(:query).and_return('%<person_bind>s')
+        allow(service).to receive(:query_format).and_return('%<person_bind>s')
         expect(service).to receive(:run_query).with('BIND(wd:Q3 AS ?person)')
         service.run
       end
@@ -44,7 +44,7 @@ RSpec.describe RetrievePositionData, type: :service do
       let(:service) { RetrievePositionData.new('Q1', 'Q2') }
 
       it 'calls run_query with no person_bind' do
-        allow(service).to receive(:query).and_return('%<person_bind>s')
+        allow(service).to receive(:query_format).and_return('%<person_bind>s')
         expect(service).to receive(:run_query).with('')
         service.run
       end

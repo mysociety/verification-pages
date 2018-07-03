9298be6aafe04d0eb6624b6d5b4e41df8ec81ccc
Graeme Porteous <graeme@rgbp.co.uk>
2018-07-03 10:02:49 +0100

    Link to Wikidata when running app locally

--- app/javascript/components/wikilink.js
+++ app/javascript/components/wikilink.js
@@ -1,5 +1,14 @@
 import template from './wikilink.html'
 
 export default template({
-  props: ['id', 'name']
+  props: ['id', 'name'],
+  computed: {
+    wikidata_site: function () {
+      if (typeof WIKIDATA_SITE !== 'undefined') {
+        return 'https://' + WIKIDATA_SITE
+      } else {
+        return ''
+      }
+    }
+  }
 })

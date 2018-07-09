34e11d7dcfdfa317acb898eadc0fef7ef62cc7c9
Graeme Porteous <graeme@rgbp.co.uk>
2018-07-09 11:09:59 +0100

    Sort statements by the persons last name

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -3,6 +3,7 @@ import ENV from './env'
 import Axios from 'axios'
 import wikidataClient from './wikiapi'
 import template from './app.html?style=./app.css'
+import { parseFullName } from 'parse-full-name'
 
 import actionWrapper from './components/action_wrapper'
 
@@ -19,12 +20,8 @@ export default template({
   },
   computed: {
     currentStatements: function () {
-      if (this.displayType !== 'all') {
-        return this.statements.filter(s => s.type === this.displayType)
-      } else {
-        return this.statements
-      }
-    },
+      return this.sortStatements(this.filterStatements(this.statements))
+    }
   },
   created: function () {
     this.loadStatements()
@@ -59,6 +56,20 @@ export default template({
       } else {
         return this.statements.length
       }
+    },
+    filterStatements: function (statements) {
+      if (this.displayType !== 'all') {
+        return statements.filter(s => s.type === this.displayType)
+      } else {
+        return statements
+      }
+    },
+    sortStatements: function (statements) {
+      return statements.sort(function (a, b) {
+        const nameA = parseFullName(a.person_name).last
+        const nameB = parseFullName(b.person_name).last
+        return nameA.localeCompare(nameB)
+      })
     }
   }
 })

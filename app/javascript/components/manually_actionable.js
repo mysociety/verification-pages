90fe3683528f672efe4c66bcef15f18bc0977fbe
Graeme Porteous <graeme@rgbp.co.uk>
2018-07-10 17:20:03 +0100

    Add Retry button to manually_actionable
    This clears reported problems which should return the statement back to
    the actionable state.

--- app/javascript/components/manually_actionable.js
+++ app/javascript/components/manually_actionable.js
@@ -1,6 +1,18 @@
+import ENV from '../env'
+import Axios from 'axios'
 import template from './manually_actionable.html'
 
 export default template({
   data () { return {} },
-  props: ['statement', 'page', 'country']
+  props: ['statement', 'page', 'country'],
+  methods: {
+    makeStatementActionable: function () {
+      this.$parent.$emit('statement-update', () => {
+        return Axios.get(
+          ENV.url + '/statements/' + this.statement.transaction_id + '.json',
+          { params: { force_type: 'actionable' } }
+        )
+      })
+    }
+  }
 })

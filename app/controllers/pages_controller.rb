449b4747d2a5326c4a3a38aaa0f30106854fdec2
Chris Mytton <chrism@mysociety.org>
2018-07-23 16:15:05 +0100

    Add reference URL title and language to pages admin UI
    This adds the appropriate fields to the show action and to the form for
    creating and updating pages.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -78,7 +78,8 @@ class PagesController < ApplicationController
     params.require(:page).permit(:title, :position_held_item,
                                  :parliamentary_term_item, :reference_url,
                                  :require_parliamentary_group, :country_id,
-                                 :csv_source_url, :executive_position)
+                                 :csv_source_url, :executive_position,
+                                :reference_url_title, :reference_url_language)
   end
 
   def new_page_params

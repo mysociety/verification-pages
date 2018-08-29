mysociety/verification-pages

commit c3eb88579310911850a54a5727af5ace3d4cd519

Skip equivalence claim for party reconciliations
Also skip if the item hasn't been changed. No specs currently around
this :/

app/models/reconciliation.rb
@@ -22,6 +22,7 @@ class Reconciliation < ApplicationRecord
   end
 
   def create_equivalence_claim
+    return if resource_type != 'person' || !item_changed?
     store.create_equivalence_claim("Added FB ID for #{statement.person_name}")
   end
 

commit 69a93bca8c6d1edb5a39a3d1052f7ce72ed16759

Fix creation of equivalence claims
Error introduced in c3eb88579310911850a54a5727af5ace3d4cd519

To check if an ActiveRecord instance has changed from an `after_commit`
callback we need to check the previous changes prior to the transaction
being committed.

This is because `changes_applied` is called [1] before our callback.
This moves the current changes in to the `@previously_changed` variable
[2] which is read by `*_previously_changed?` predicates.

[1] https://github.com/rails/rails/blob/6eaae62/activerecord/lib/active_record/attribute_methods/dirty.rb#L19-L20
[2] https://github.com/rails/rails/blob/6eaae62/activemodel/lib/active_model/dirty.rb#L146

app/models/reconciliation.rb
@@ -63,7 +63,7 @@ class Reconciliation < ApplicationRecord
   end
 
   def create_equivalence_claim
-    return if resource_type != 'person' || !item_changed?
+    return if resource_type != 'person' || !item_previously_changed?
     store.create_equivalence_claim("Added FB ID for #{statement.person_name}")
   end
 

spec/models/reconciliation_spec.rb
@@ -234,5 +234,26 @@ RSpec.describe Reconciliation, type: :model do
         end
       end
     end
+
+    context 'statement has FB identifier' do
+      let(:statement) do
+        # set initial person_item otherwise statement will attempt to fetch it
+        # from ID mapping store
+        create(:statement, person_item: 'Q1', fb_identifier: 'ABC')
+      end
+
+      before { reconciliation.resource_type = 'person' }
+
+      it 'should create equivalence claim' do
+        store = double('IDMappingStore')
+        expect(IDMappingStore).to receive(:new)
+          .with(wikidata_id: 'Q123', facebook_id: 'ABC')
+          .and_return(store)
+        expect(store).to receive(:create_equivalence_claim)
+
+        reconciliation.item = 'Q123'
+        reconciliation.save!
+      end
+    end
   end
 end
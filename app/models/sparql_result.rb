4952064a7f0519868ee2f5b60159840bd8d10690
Graeme Porteous <graeme@rgbp.co.uk>
2018-10-15 16:15:40 +0100

    Change SparqlResult into module
    Extend the result Hash from the query to provide convenience methods and
    allows other Hash methods to be used.

--- app/models/sparql_result.rb
+++ app/models/sparql_result.rb
@@ -1,32 +1,33 @@
 # frozen_string_literal: true
 
-# Object representing the result of a SPARQL query
-class SparqlResult
-  def initialize(raw_hash, variables)
-    @raw = raw_hash
-    @variables = variables
-  end
+# Module with convenience methods for accessing and mapping results of
+# difference data types from data returned from a SPARQL query
+module SparqlResult
+  attr_writer :variables
 
-  def [](attr)
-    value = @raw[attr]
+  def [](*args)
+    value = super
     return nil unless value
     map_value(value)
   end
 
   private
 
+  def variables
+    @variables || keys
+  end
+
   def respond_to_missing?(*args)
     super
   end
 
   def method_missing(attr, *args)
-    return super unless @variables.include?(attr.to_s)
+    return super unless variables.include?(attr.to_s)
     self[attr]
   end
 
   def map_value(h)
     return h[:value].to_s[0..9] if h[:datatype] == 'http://www.w3.org/2001/XMLSchema#dateTime'
-
     return h[:value].to_s.split('/').last if h[:type] == 'uri'
 
     h[:value]

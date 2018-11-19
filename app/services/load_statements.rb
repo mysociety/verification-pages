c41454fc8cbd5b9d9dd9e67ceab5f66d30df3d8a
Graeme Porteous <graeme@rgbp.co.uk>
2018-11-19 09:32:49 +0000

    Introduce Row objects for loading statements
    No changes to functionality but this extraction will make it easier for
    us to add aliases to the row column names.

--- app/services/load_statements.rb
+++ app/services/load_statements.rb
@@ -12,7 +12,7 @@ class LoadStatements < ServiceBase
 
   def run
     Statement.transaction do
-      touched_statements = csv.map { |result| parse_result(result) }
+      touched_statements = csv.map { |data| Row.new(data, page: page).parse }
       untouched_statements = page.statements.where.not(id: touched_statements)
       untouched_statements.update(removed_from_source: true)
       touched_statements.each(&:save!)
@@ -32,14 +32,22 @@ class LoadStatements < ServiceBase
   rescue RestClient::Exception => e
     raise "Suggestion store failed: #{e.message}"
   end
+end
 
-  def parse_result(result)
-    result[:transaction_id] ||= page.generate_transaction_id(result.to_h)
+class Row < OpenStruct
+  attr_reader :data, :page
 
-    statement = page.statements.find_or_initialize_by(
-      transaction_id: result[:transaction_id]
-    )
+  def initialize(data, page:)
+    @data = data.to_h
+    @page = page
+    super(@data)
+  end
 
+  def transaction_id
+    super || page.generate_transaction_id(data)
+  end
+
+  def parse
     # We need to be careful not wipe out any manually reconciled
     # items when refreshing from the upstream CSV file, so don't
     # overwrite the *_item attributes if that'd make them blank:
@@ -48,22 +56,30 @@ class LoadStatements < ServiceBase
       reconciliation = statement.reconciliations
                                 .where(resource_type: type)
                                 .last
-      value = reconciliation ? reconciliation.item : result[attribute]
+      value = reconciliation ? reconciliation.item : public_send(attribute)
       statement.public_send("#{attribute}=", value) if value.present?
     end
 
     # The other attributes we always update from the upstream CSV:
     statement.assign_attributes(
       page:                     page,
-      person_name:              result[:person_name],
-      electoral_district_name:  result[:electoral_district_name],
-      parliamentary_group_name: result[:parliamentary_group_name],
-      fb_identifier:            result[:fb_identifier],
-      position_start:           result[:position_start],
-      position_end:             result[:position_end],
+      person_name:              person_name,
+      electoral_district_name:  electoral_district_name,
+      parliamentary_group_name: parliamentary_group_name,
+      fb_identifier:            fb_identifier,
+      position_start:           position_start,
+      position_end:             position_end,
       removed_from_source:      false
     )
 
     statement
+  end
+
+  private
+
+  def statement
+    @statement ||= page.statements.find_or_initialize_by(
+      transaction_id: transaction_id
+    )
   end
 end

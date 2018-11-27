3ed957c5b7d553a8b1d5a079bd3f814a76b282e3
Graeme Porteous <graeme@rgbp.co.uk>
2018-11-27 09:19:10 +0000

    Add header navigation and flash to layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@
 
 class ApplicationController < ActionController::Base # :nodoc:
   protect_from_forgery with: :exception
+
+  helper_method :authenticated?
+
+  def authenticated?
+    false
+  end
 end

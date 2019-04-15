c8d5dd462f5f3733bebbe0c625b3c08b93311acb
Graeme Porteous <graeme@rgbp.co.uk>
2019-04-15 09:44:11 +0100

    Parse dates from SPARQL queries
    This ensures when comparing dates in the membership-comparison gem we're
    comparing dates with dates, instead of dates with strings.

--- spec/services/retrieve_position_data_spec.rb
+++ spec/services/retrieve_position_data_spec.rb
@@ -26,14 +26,14 @@ RSpec.describe RetrievePositionData, type: :service do
   describe '#run' do
     it 'calls run_query with substituted position_held_items' do
       allow(service).to receive(:query_format).and_return('%<position_held_items>s')
-      expect(service).to receive(:run_query).with('(wd:Q1)')
+      expect(service).to receive(:run_query).with('(wd:Q1)').and_return([])
       service.run
     end
 
     context 'with person_item' do
       it 'calls run_query with substituted person_bind' do
         allow(service).to receive(:query_format).and_return('%<person_bind>s')
-        expect(service).to receive(:run_query).with('BIND(wd:Q2 AS ?person)')
+        expect(service).to receive(:run_query).with('BIND(wd:Q2 AS ?person)').and_return([])
         service.run
       end
     end
@@ -43,9 +43,22 @@ RSpec.describe RetrievePositionData, type: :service do
 
       it 'calls run_query with no person_bind' do
         allow(service).to receive(:query_format).and_return('%<person_bind>s')
-        expect(service).to receive(:run_query).with('')
+        expect(service).to receive(:run_query).with('').and_return([])
         service.run
       end
+    end
+
+    it 'type cast string date values' do
+      input = {
+        position_start: '2019-01-01', position_end: '2019-02-01',
+        term_start: '2018-01-01', term_end: '2018-02-01',
+      }
+      output = {
+        position_start: Date.new(2019, 1, 1), position_end: Date.new(2019, 2, 1),
+        term_start: Date.new(2018, 1, 1), term_end: Date.new(2018, 2, 1),
+      }
+      allow(service).to receive(:run_query).and_return([input])
+      expect(service.run).to match_array([output])
     end
   end
 end
